ee25c1aea01608171dbc923e164efe490b276cfa
jameswilson281 <jamestwilson@live.com>
2017-09-12 17:54:22 -0700

    sample program

---
+++ Assignments/Due 09-18-2017/Sample/Sample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "continue";
+            while (input != "q")
+            {
+                Console.Clear();
+                Console.Write("Enter an integer (or 'q' to exit): ");
+                input = Console.ReadLine();
+                if (input == "q")
+                {
+                    return;
+                }
+                if (int.TryParse(input, out int theInteger) == false)
+                {
+                    Console.WriteLine($"{input} is not a valid integer!");
+
+                }
+                else
+                {
+                    var even = (theInteger % 2 == 0);
+                    Console.WriteLine(even ? $"{theInteger} is even" : $"{theInteger} is odd");
+                }
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
